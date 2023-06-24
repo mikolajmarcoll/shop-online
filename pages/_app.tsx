3121fb6a7042976882df0f6c17d137b1d2e7a439
mikolaj-marcoll <mikolaj.marcoll@htdevelopers.com>
2023-06-24 11:12:49 +0200

    add react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const client = new QueryClient()
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <QueryClientProvider client={client}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
+)
+
+export default App
